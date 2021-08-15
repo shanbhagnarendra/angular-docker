a9704a63ed94f6ef926c14b37d19d55c54f78074
Narendra Shanbhag <shanbhag.narendra@gmail.com>
2021-08-15 08:23:45 +0530

    Test: Updated karma config and test command

--- karma.conf.js
+++ karma.conf.js
@@ -23,7 +23,7 @@ module.exports = function (config) {
     },
     reporters: ['progress', 'kjhtml', 'junit'],
     junitReporter: {
-      outputDir: '', // results will be saved as $outputDir/$browserName.xml
+      outputDir: './test-results', // results will be saved as $outputDir/$browserName.xml
       outputFile: undefined, // if included, results will be saved as $outputDir/$browserName/$outputFile
       suite: '', // suite will become the package name attribute in xml testsuite element
       useBrowserName: true, // add browser name to report and classes names
